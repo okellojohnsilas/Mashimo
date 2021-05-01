8e79712abbc49c00a624c6bad2bf768fed08307f
okello.john@strathmore.edu <okello.john@strathmore.edu>
2021-05-02 00:18:54 +0300

    Complete the services

---
+++ js/mashimo.js
@@ -0,0 +1,16 @@
+// Search card title 
+function searchCardTitle() {
+    var input, filter, cards, cardContainer, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("mashimo-services");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-title");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
